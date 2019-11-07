ac936ffa8ef630af1f092c442e2ff05e3b213e8f
David Salas <dsf.2528@gmail.com>
2019-11-07 17:54:32 +0100

    lista productos

--- app/src/main/java/com/example/barreinolds/ListaCategorias.java
+++ app/src/main/java/com/example/barreinolds/ListaCategorias.java
@@ -2,29 +2,51 @@ package com.example.barreinolds;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
 
 import java.util.ArrayList;
 
 public class ListaCategorias extends AppCompatActivity {
 
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_categorias);
+
+        getSupportActionBar().setTitle("Bar Reinolds");
+
         ArrayList<String> categorias = listaCategorias();
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, categorias);
-        ListView listView = (ListView) findViewById(R.id.lista);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, categorias);
+
+        listView = (ListView) findViewById(R.id.lista);
         listView.setAdapter(itemsAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(ListaCategorias.this, ListaProductos.class);
+                intent.putExtra("Categoria", listView.getItemAtPosition(position).toString());
+                startActivity(intent);
+            }
+        });
     }
 
     private ArrayList<String> listaCategorias() {
-        ArrayList<String> categorias =  new ArrayList<String>();
+        ArrayList<String> categorias = new ArrayList<String>();
+        categorias.clear();
         categorias.add("Bebidas");
         categorias.add("Carnes");
         categorias.add("Postres");
         return categorias;
     }
+
 }
---
+++ app/src/main/java/com/example/barreinolds/ListaProductos.java
@@ -0,0 +1,52 @@
+package com.example.barreinolds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ListaProductos extends AppCompatActivity {
+
+    ImageView imgProducto;
+    ListView listView;
+    TextView titulo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_productos);
+        titulo = findViewById(R.id.nombre_productos);
+        getSupportActionBar().setTitle("Bar Reinolds");
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            titulo.setText(bundle.getString("Categoria"));
+            ArrayList<String> categorias = listaProductos(bundle.getString("Categoria"));
+            ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, categorias);
+            listView = (ListView) findViewById(R.id.lista);
+            listView.setAdapter(itemsAdapter);
+        }
+
+    }
+
+    private ArrayList<String> listaProductos(String categoria) {
+        ArrayList<String> productos = new ArrayList<String>();
+        productos.clear();
+        if (categoria.equalsIgnoreCase("postres")) {
+            productos.add("Cafe");
+            productos.add("Pastel");
+            productos.add("Helado");
+        } else if (categoria.equalsIgnoreCase("bebidas")) {
+            productos.add("Cocacola");
+            productos.add("Fanta");
+            productos.add("Vino");
+        }
+        return productos;
+    }
+}
