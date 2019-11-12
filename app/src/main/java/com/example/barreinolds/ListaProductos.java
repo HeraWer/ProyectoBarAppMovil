f8b417cc912eaf523899f509c989ae3165077af0
David Salas <dsf.2528@gmail.com>
2019-11-12 17:41:55 +0100

    botones pedido

--- app/src/main/java/com/example/barreinolds/ListaProductos.java
+++ app/src/main/java/com/example/barreinolds/ListaProductos.java
@@ -3,6 +3,7 @@ package com.example.barreinolds;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -10,24 +11,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import java.util.ArrayList;
 
 import static com.example.barreinolds.ListaCategorias.lp;
+import static com.example.barreinolds.Mesas.numMesa;
 
 public class ListaProductos extends AppCompatActivity {
 
     ListView listView;
     TextView titulo;
     ArrayList<Product> productos;
+    Ticket ticket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_productos);
+
+        ticket = new Ticket();
+        ticket.setMesa(numMesa);
+
         titulo = findViewById(R.id.nombre_productos);
         getSupportActionBar().setTitle("Bar Reinolds");
 
@@ -35,8 +44,6 @@ public class ListaProductos extends AppCompatActivity {
         if (bundle != null) {
             titulo.setText(bundle.getString("Categoria"));
             productos = lp;
-//            ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this,
-//                    android.R.layout.simple_list_item_1, categorias);
 
             CustomAdapter customAdapter = new CustomAdapter(this, productos);
             listView = (ListView) findViewById(R.id.lista_productos);
@@ -56,7 +63,7 @@ public class ListaProductos extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            Product producto = getItem(position);
+            final Product producto = getItem(position);
             if (convertView == null) {
                 LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 convertView = inflater.inflate(R.layout.customlayout_productos, parent, false);
@@ -64,13 +71,41 @@ public class ListaProductos extends AppCompatActivity {
                 ImageView imgProducto = (ImageView) convertView.findViewById(R.id.imagen_producto);
                 TextView nombreProducto = convertView.findViewById(R.id.nombre_producto);
                 TextView precioProducto = convertView.findViewById(R.id.precio_producto);
+                Button restarProducto = convertView.findViewById(R.id.boton_resta);
+                Button sumarProducto = convertView.findViewById(R.id.boton_suma);
+                final TextView cantidadProducto = convertView.findViewById(R.id.cantidad_producto);
+
+                restarProducto.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (producto.getCantidad() > 0) {
+                            producto.setCantidad(producto.getCantidad() - 1);
+                            cantidadProducto.setText(String.valueOf(producto.getCantidad()));
+                            if (producto.getCantidad() <= 0) {
+                                ticket.getProductosComanda().remove(producto);
+                            }
+                        }
+                    }
+                });
+
+                sumarProducto.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        producto.setCantidad(producto.getCantidad() + 1);
+                        cantidadProducto.setText(String.valueOf(producto.getCantidad()));
+                        if (ticket.getProductosComanda().contains(producto)) {
+                            ticket.getProductosComanda().set(ticket.getProductosComanda().indexOf(producto), producto);
+                        } else {
+                            ticket.getProductosComanda().add(producto);
+                        }
+                    }
+                });
 
                 String uri = producto.getImage();
                 int imageResource = getResources().getIdentifier(uri, "drawable", getPackageName());
                 Drawable imagenDra = ContextCompat.getDrawable(getContext(), imageResource);
 
                 imgProducto.setImageDrawable(imagenDra);
-//                imgProducto.setImageResource(imageResource);
                 nombreProducto.setText(producto.getName());
                 precioProducto.setText(producto.getPrice() + "€");
             }
--- app/src/main/java/com/example/barreinolds/Ticket.java
+++ app/src/main/java/com/example/barreinolds/Ticket.java
@@ -7,5 +7,28 @@ public class Ticket {
     private ArrayList<Product> productosComanda;
     private int mesa;
 
+    public Ticket() {
+        productosComanda = new ArrayList<Product>();
+    }
 
+    public Ticket(ArrayList<Product> productosComanda, int mesa) {
+        this.productosComanda = productosComanda;
+        this.mesa = mesa;
+    }
+
+    public int getMesa() {
+        return mesa;
+    }
+
+    public void setMesa(int mesa) {
+        this.mesa = mesa;
+    }
+
+    public ArrayList<Product> getProductosComanda() {
+        return productosComanda;
+    }
+
+    public void setProductosComanda(ArrayList<Product> productosComanda) {
+        this.productosComanda = productosComanda;
+    }
 }
