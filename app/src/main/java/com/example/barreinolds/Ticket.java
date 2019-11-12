34164912732d15f81ee5df494d32072ea41060aa
Erik Cabezuelo <erikcf95@gmail.com>
2019-11-12 17:41:23 +0100

    ServerConnection

--- app/src/main/java/com/example/barreinolds/Ticket.java
+++ app/src/main/java/com/example/barreinolds/Ticket.java
@@ -1,8 +1,9 @@
 package com.example.barreinolds;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Ticket {
+public class Ticket implements Serializable {
 
     private ArrayList<Product> productosComanda;
     private int mesa;
