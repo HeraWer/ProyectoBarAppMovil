498e6f8191dcfe109da8e833b2b9ad53f6d716ed
Erik Cabezuelo <erikcf95@gmail.com>
2019-11-18 17:13:54 +0100

    Actualizado ticket

---
+++ app/src/main/java/com/example/barreinolds/Camarero.java
@@ -0,0 +1,70 @@
+package com.example.barreinolds;
+
+import java.util.ArrayList;
+
+/*
+ * Esta es la clase que define a los camareros.
+ */
+public class Camarero {
+
+    // Atributos de la clase
+    private int id;
+    private String nombre;
+    private String username;
+    private ArrayList<Integer> mesas;
+
+    /*
+     * Constructor vacío.
+     */
+    public Camarero() {
+        mesas = new ArrayList<Integer>();
+    }
+
+    /*
+     * Constructor en el que se le pasa el nombre y el numero
+     * de mesa y se le asigna a sus atributos.
+     */
+    public Camarero(int id, String nombre, String username, ArrayList<Integer> mesas) {
+        super();
+        this.id = id;
+        this.nombre = nombre;
+        this.username = username;
+        this.mesas = mesas;
+    }
+
+    /*
+     * Getters y setters
+     */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Integer> getMesas() {
+        return mesas;
+    }
+
+    public void setMesas(ArrayList<Integer> mesas) {
+        this.mesas = mesas;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
